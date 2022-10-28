f625bb156e95ce550b2c01c49bc73b0e089d9d08
Danila Nefedov <dn375@mail.ru>
2022-10-28 14:54:55 +0400

    created component AddTask

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import Home from "@/views/Home/TheHome.vue";
+import AddTask from "@/views/AddTask/AddTask.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "Home",
     component: Home,
+  },
+
+  {
+    path: "/add_task",
+    name: "AddTask",
+    component: AddTask,
   },
 ];
 
